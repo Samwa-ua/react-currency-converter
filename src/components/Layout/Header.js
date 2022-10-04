fd1ba36c65807dfe2df8bddc4698da7e56121f0e
Andrii Skokovskyi <askokovskiy@gmail.com>
2022-10-04 17:27:06 +0300

    ref:Separate logic from UI in header component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,24 +3,24 @@ import useFetch from "../../hooks/useFetch";
 
 import styles from "./Header.module.css";
 
-const Header = () => {
-  const { data: exchangeRate, isPending } = useFetch(
-    "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json"
-  );
+import { nbuUrl } from "../../constants/urls";
+import { filterCurrency } from "../../helpers/filteredNbuCurrencyRate";
 
-  const currencyRate = exchangeRate
-    .filter((currency) => currency.cc === "USD" || currency.cc === "EUR")
-    .map((currency) => (
-      <div>
-        <p key={currency.cc}>
-          {currency.cc}/UAH <span>{currency.rate}</span>
-        </p>
-      </div>
-    ));
+const Header = () => {
+  const { data: exchangeRate, isPending } = useFetch(nbuUrl);
+
+  const currencyRate = filterCurrency(exchangeRate).map((currency) => (
+    <div>
+      <p key={currency.cc}>
+        {currency.cc}/UAH <span>{currency.rate}</span>
+      </p>
+    </div>
+  ));
+
   return (
     <header className={styles.header}>
       <h2>Exchange Currency Converter</h2>
-      {isPending && <p>Loading</p>}
+      {isPending && <p>Loading...</p>}
       <div className={styles.currencyRate}>{currencyRate}</div>
     </header>
   );
---
+++ src/constants/urls.js
@@ -0,0 +1,2 @@
+export const nbuUrl =
+  "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
